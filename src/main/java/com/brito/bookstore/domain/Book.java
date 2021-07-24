a698b8851f3f4d192e6e2000ac633d94f7ed9389
Jonathan-Brito <devjonathanbrito@gmail.com>
2021-07-24 16:42:58 -0300

    Configuration entities

--- src/main/java/com/brito/bookstore/domain/Book.java
+++ src/main/java/com/brito/bookstore/domain/Book.java
@@ -1,10 +1,22 @@
 package com.brito.bookstore.domain;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
-public class Book {
+public class Book implements Serializable {
 	
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
 	private String title;
@@ -13,6 +25,8 @@ public class Book {
 	
 	private String text;
 	
+	@ManyToOne
+	@JoinColumn(name = "category_id")
 	private Category category;
 
 	public Book() {
--- src/main/java/com/brito/bookstore/domain/Category.java
+++ src/main/java/com/brito/bookstore/domain/Category.java
@@ -1,19 +1,30 @@
 package com.brito.bookstore.domain;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
-public class Category {
+public class Category implements Serializable {
 
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 
 	private String name;
 
 	private String description;
-
+	
+	@OneToMany(mappedBy = "category")
 	private List<Book> books = new ArrayList<>();
 
 	public Category() {
