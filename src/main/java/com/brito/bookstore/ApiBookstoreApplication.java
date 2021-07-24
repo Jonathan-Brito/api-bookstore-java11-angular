33d00d7c66b9851e27ff4dcef452c4e6b938155d
Jonathan-Brito <devjonathanbrito@gmail.com>
2021-07-24 15:49:45 -0300

    Project created

---
+++ src/main/java/com/brito/bookstore/ApiBookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.brito.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBookstoreApplication.class, args);
+	}
+
+}
